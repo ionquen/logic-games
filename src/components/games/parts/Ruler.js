33c126117d3a6c5a1c7b471021fd2bfc269b0519
ionquen <ionquen@pm.me>
2021-01-21 18:16:25 +0300

    add ruler styles

---
+++ src/components/games/parts/Ruler.js
@@ -0,0 +1,28 @@
+import React from 'react'
+/*
+  displayCurrentPlayer - Выделить текущего игрока
+*/
+export default function Ruler(props) {
+  function getListByLength(length) {
+    let section = []
+    for(let i = 0; i < length; i++) {
+      section.push(<li></li>)
+    }
+    return section
+  }
+  return (
+    <div className={props.className} >
+        <ol>
+          {getListByLength(props.countX)}
+        </ol>
+        <ol>
+          {getListByLength(props.countY)}
+        </ol>
+        <div>
+          <div></div>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
