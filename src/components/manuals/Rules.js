f508292673838f759fd26b2919a9e3158434da57
ionquen <ionquen@pm.me>
2021-01-31 17:59:23 +0300

    fix text in rules popup

--- src/components/manuals/Rules.js
+++ src/components/manuals/Rules.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from '../../static/css/manual.module.css'
-import rulesBloom from '../../static/img/rulesBloom.jpeg'
 
 export default function ManualTictactoe(props) {
   return(
     <article className={styles.manual}>
-      <h1>Правила пользования сервисом</h1>
-      <img className={styles.imgFull} src={rulesBloom} alt="Very important rules" />
+      <h1>Правила</h1>
+      <section>
+        <h2>Общее положение</h2>
+        <p>Уважаемые посетители ресурса! Наш сайт предоставляет возможность <span className={styles.select}>бесплатно</span> и <span className={styles.select}>без ограничений</span> играть в логические онлайн игры. Такая возможность лишает нас контроля над текущим состоянием ресурса, в том числе модерации сообщений в чатах и контроля выделяемых ресурсов для каждого пользователя. Мы не можем нести ответственность за контент, <span className={styles.select}>размещаемый нашими посетителями</span>, а также <span className={styles.select}>бесперебойную работу ресурса</span>.</p>
+        <p>Поэтому, мы <span className={styles.select}>просим наших посетителей</span> придерживаться следующих правил:</p>
+        <ul>
+          <li>В глобальном чате не использовать <span className={styles.select}>нецензурную лексику</span> и <span className={styles.select}>не оскорблять</span> кого бы то ни было </li>
+          <li>Не совершать DOS и DDOS атаки. Наш сервер работает только благодаря энтузиазму разработчика. Роняя его вы лишаете других пользователей ресурса, созданного не ради каких-либо корыстных целей </li>
+        </ul>
+      </section>
     </article>
   )
 }
